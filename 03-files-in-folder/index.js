20a4f0f53d21edc11e96aee388f51ba683a7bd69
Siarhei Prymakovich <prima.psa@gmail.com>
2022-05-20 19:52:46 +0300

    feat: implement 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const fs = require('fs');
+const folderPath = path.resolve(__dirname, 'secret-folder');
+fs.promises.readdir(folderPath).then((list) => {
+  if(!list.length) return;
+  list.forEach((file)=>{
+    if(file && /.*\..{2,}/gi.test(file)){
+      let filePath = path.join(folderPath, file);      
+      fs.stat(filePath, (err, info)=>{
+        if(!err && !info.isDirectory()){
+          let fileSize = info.size; 
+          let fileExtension = path.extname(filePath).substring(1);
+          let fileName = path.basename(filePath);         
+          fileName = fileName.substring(0, fileName.lastIndexOf('.'));          
+          process.stdout.write(`${fileName} - ${fileExtension} - ${fileSize} bytes\r\n`);
+        }        
+      });
+    }   
+  });  
+});
+   
+
+
