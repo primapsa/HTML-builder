54e059e444ea5117ace179396d2dc370d5c51f72
Siarhei Prymakovich <prima.psa@gmail.com>
2022-05-20 14:31:47 +0300

    feat: implement 01-read-file

--- 01-read-file/index.js
+++ 01-read-file/index.js
@@ -1,3 +1,9 @@
 const libFs = require('fs');
-
-
+const libPath = require('path');
+const filename = 'text.txt';
+const path = libPath.resolve(__dirname, filename);
+const stdout = process.stdout;
+const handle = libFs.ReadStream(path, 'utf-8');
+let fullData = '';
+handle.on('data', data => fullData += data);
+handle.on('end', () => stdout.write(fullData));
