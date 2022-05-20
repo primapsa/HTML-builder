2be3f8d39c79596b35fc42fd9ae0c7d352056a43
Siarhei Prymakovich <prima.psa@gmail.com>
2022-05-20 23:57:17 +0300

    fix: clean folder before copy

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,6 +2,15 @@ const path = require('path');
 const fs = require('fs');
 const folderDest = path.join(__dirname, 'files-copy');
 const folderSrc = path.join(__dirname, 'files');
+fs.stat(folderDest, err =>{
+  if(err) return;
+  fs.promises.readdir(folderDest).then((list) =>{
+    if(!list.length) return;
+    list.forEach((file)=>{
+      fs.unlink(path.join(folderDest, file),(err) => err);
+    });
+  });
+});
 fs.mkdir(folderDest,{ recursive: true}, err =>{
   if(err) throw new Error(err);
   fs.promises.readdir(folderSrc).then((list) => { 
