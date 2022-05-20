1adffeda8c66d4cd3d36a357cd683f642be89817
Siarhei Prymakovich <prima.psa@gmail.com>
2022-05-20 20:56:52 +0300

    feat: implement 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,13 @@
-
+const path = require('path');
+const fs = require('fs');
+const folderDest = path.join(__dirname, 'files-copy');
+const folderSrc = path.join(__dirname, 'files');
+fs.mkdir(folderDest,{ recursive: true}, err =>{
+  if(err) throw new Error(err);
+  fs.promises.readdir(folderSrc).then((list) => { 
+    if(!list.length) return;
+    list.forEach((file)=> {
+      if(file) fs.promises.copyFile(path.join(folderSrc,file), path.join(folderDest, file));        
+    });  
+  });
+});
