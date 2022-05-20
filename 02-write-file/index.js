15bb5521bd504fa020645c6da9f1815ef63a754b
Siarhei Prymakovich <prima.psa@gmail.com>
2022-05-20 16:09:29 +0300

    feat: implement 02-write-file

---
+++ 02-write-file/index.js
@@ -0,0 +1,16 @@
+const fs = require('fs');
+const path = require('path');
+const filename = 'text.txt';
+const filePath = path.resolve(__dirname, filename);
+const fileHandler = fs.WriteStream(filePath, 'utf-8');
+const {stdin, stdout} = process;
+stdout.write('Write something, please...\n');
+stdin.on('data', (data)=>{
+  data = data.toString();  
+  if(!data) return;
+  if(data === 'exit\r\n'){
+    process.exit();
+  }else{
+    fileHandler.write(data);}    
+});
+process.on('exit', ()=>stdout.write('Good work! Bye!'));
