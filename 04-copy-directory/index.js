9a1df76526eaff398f1552752e212d72f63033b7
Siarhei Prymakovich <prima.psa@gmail.com>
2022-05-23 16:45:13 +0300

    fix: fix folder del bug

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -2,19 +2,23 @@ const path = require('path');
 const fs = require('fs');
 const folderDest = path.join(__dirname, 'files-copy');
 const folderSrc = path.join(__dirname, 'files');
-fs.stat(folderDest, err =>{
-  if(err) return;
-  fs.promises.readdir(folderDest).then((list) =>{
-    if(!list.length) return;
-    list.forEach((file)=>{
-      fs.unlink(path.join(folderDest, file),(err) => err);
-    });
-  });
-});
-fs.mkdir(folderDest,{ recursive: true}, err =>{
-  if(err) throw new Error(err);
-  copyFolder(folderSrc, folderDest);
-});
+
+prepareCopy();
+async function  prepareCopy() {
+  const isFolder = await fs.promises.stat(folderDest);
+  if(isFolder){
+    const files = await fs.promises.readdir(folderDest);
+    if(files.length){
+      files.forEach(async file => {
+        await fs.promises.rm(path.join(folderDest, file),{recursive: true, force: true, maxRetries: 5});
+      });
+    }
+    copyFolder(folderSrc, folderDest);    
+  }
+  else{
+    fs.promises.mkdir(folderDest,{recursive: true});
+  }
+}
 
 function copyFolder(src, dest){
   fs.promises.readdir(src).then( list => {
